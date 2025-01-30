77a441a501a4fd6af1fec9ef1fdba660419a789a
NosenkoArtem <nosenkoartyom1989@gmail.com>
2025-01-30 16:58:16 +0300

    [REFACT] Add new info 1.7.0.py

--- 1.7.0.py
+++ 1.7.0.py
@@ -21,19 +21,30 @@ print(coord)
 class MyVector:
     MIN_СOORD, MAX_COORD = 0, 1_000
     def __init__(self, x, y):
-        if MyVector.validate(x, y):
+        if MyVector.validate(x) and MyVector.validate(y):
             self.x = x
             self.y = y
+            self.norm_vector = self.norm2(self.x, self.y)
+        else:
+            raise ValueError('Некорректные значения переменных')
  
     def get_coord(self):
         return self.x, self.y
     
+    # нет доступа к атрибутам экземпляра 
+    # есть доступ к атрибутам класса
     @classmethod
-    def validate(cls, x:float, y:float):
-        if cls.MIN_СOORD <=x<= cls.MAX_COORD and cls.MIN_СOORD <=y<= cls.MAX_COORD:
+    def validate(cls, arg:float):
+        if cls.MIN_СOORD <=arg<= cls.MAX_COORD:
             return True
         else:
-            ValueError
+            return False
+    
+    # нет доступа к атрибутам экземпляра 
+    # и класса
+    @staticmethod    
+    def norm2(x, y):
+        return x*x + y*y
             
-my_v = MyVector(-1, 2)
+my_v = MyVector(100, 2)
 print(my_v)
